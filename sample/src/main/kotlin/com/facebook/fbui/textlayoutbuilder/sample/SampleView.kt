e8c8ff6bc3674c877b6c2e49d17464e4d58bc999
Hilal Alsibai <halsibai@fb.com>
2017-12-14 11:28:14 -0800

    Add Licensing for Sample Code
    Summary: Missed these
    
    Reviewed By: sjkirby
    
    Differential Revision: D6565962
    
    fbshipit-source-id: 8c4bb83f6832864c579834f0d992024b274052aa

--- sample/src/main/kotlin/com/facebook/fbui/textlayoutbuilder/sample/SampleView.kt
+++ sample/src/main/kotlin/com/facebook/fbui/textlayoutbuilder/sample/SampleView.kt
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ * 
+ * This source code is licensed under the license found in the
+ * LICENSE-examples file in the root directory of this source tree.
+ */
 package com.facebook.fbui.textlayoutbuilder.sample
 
 import android.content.Context
