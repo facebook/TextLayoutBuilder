a870c10661a05c5f71de0c7474b08b677b676d87
Hilal Alsibai <xiphirx@users.noreply.github.com>
2017-12-13 20:56:10 -0800

    Add Sample App
    This gives us a small playground to try out
    different things without having to re-setup
    a dev environment continuously. It also can
    serve as a showcase of how the library can
    be used.

---
+++ sample/src/main/kotlin/com/facebook/fbui/textlayoutbuilder/sample/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.facebook.fbui.textlayoutbuilder.sample
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Layout
+import android.util.TypedValue
+import android.widget.LinearLayout
+import com.facebook.fbui.textlayoutbuilder.TextLayoutBuilder
+
+class MainActivity : AppCompatActivity() {
+  private lateinit var parent: LinearLayout
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    parent = findViewById(R.id.parent)
+
+    addSample {
+      TextLayoutBuilder().setText("Hello, world!").setTextSize(20f.dp(this)).build()
+    }
+  }
+
+  private fun addSample(block: () -> Layout) {
+    val layout = block()
+    parent.addView(SampleView(this, layout).apply {
+      layoutParams = LinearLayout.LayoutParams(layout.width, layout.height)
+    })
+  }
+
+  private fun Float.dp(context: Context): Int {
+    return Math.round(
+        TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, this, context.resources.displayMetrics))
+  }
+}
